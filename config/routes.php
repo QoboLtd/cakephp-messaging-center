d8db5b04a3e63d8056710688a6e0785e7270c5c3
George Constantinou <g.constantinou@qobo.biz>
2016-03-16 20:04:51 +0200

    change default action to 'inbox' (task #1495)

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,7 @@ Router::plugin(
     'MessagingCenter',
     ['path' => '/messaging-center'],
     function ($routes) {
+        $routes->connect('/messages/', ['controller' => 'Messages', 'action' => 'inbox']);
         $routes->fallbacks('DashedRoute');
     }
 );
