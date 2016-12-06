0033509f7e80bd6ecd61e4768715325f8d4abf0e
George Constantinou <g.constantinou@qobo.biz>
2016-12-06 19:27:23 +0200

    write configuration parameter for system user (task #2719)

---
+++ config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+use Cake\Core\Configure;
+
+Configure::write('MessagingCenter', [
+    'systemUser' => [
+        'id' => '00000000-0000-0000-0000-000000000000',
+        'name' => 'SYSTEM'
+    ]
+]);
