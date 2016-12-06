8fe698bfac1dff814fc05927c458a3b07637c6b4
George Constantinou <g.constantinou@qobo.biz>
2016-12-06 19:38:38 +0200

    moved configuration parameters to another file (task #2719)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
 use Cake\Core\Configure;
 
-Configure::write('MessagingCenter', [
-    'systemUser' => [
-        'id' => '00000000-0000-0000-0000-000000000000',
-        'name' => 'SYSTEM'
-    ]
-]);
+Configure::load('MessagingCenter.messaging_center');
