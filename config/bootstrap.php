bc17f90b9837c0bf07f3e3ea723d74349dd4e46b
George Constantinou <g.constantinou@qobo.biz>
2016-12-07 11:56:50 +0200

    Plugin Configuration (task #2719)
    Prevent overwritting of app level config by the plugin config, if
    the app level config was loaded before the plugin.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,4 +1,14 @@
 <?php
 use Cake\Core\Configure;
 
+// get app level config
+$config = Configure::read('MessagingCenter');
+
+// load default plugin config
 Configure::load('MessagingCenter.messaging_center');
+
+// overwrite default plugin config by app level config
+Configure::write('MessagingCenter', array_replace_recursive(
+    Configure::read('MessagingCenter'),
+    $config
+));
