85e80fc8a703d0752e70612e1f27be21f634ec0b
George Constantinou <g.constantinou@qobo.biz>
2016-12-09 19:22:27 +0200

    set config parameters for api token and typeahead options (task #3175)

--- config/messaging_center.php
+++ config/messaging_center.php
@@ -5,6 +5,13 @@ return [
         'systemUser' => [
             'id' => '00000000-0000-0000-0000-000000000000',
             'name' => 'SYSTEM'
+        ],
+        'typeahead' => [
+            'min_length' => 1,
+            'timeout' => 300
+        ],
+        'api' => [
+            'token' => null
         ]
     ]
 ];
