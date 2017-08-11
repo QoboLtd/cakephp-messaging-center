73f0c5fa287b3d83e230bfd7a430482589fe4656
George Constantinou <georgec20001@gmail.com>
2017-08-11 11:03:02 +0300

    Unit tests for messages table (task #4340)

--- tests/TestCase/Model/Table/MessagesTableTest.php
+++ tests/TestCase/Model/Table/MessagesTableTest.php
@@ -1,8 +1,11 @@
 <?php
 namespace MessagingCenter\Test\TestCase\Model\Table;
 
+use Cake\I18n\Time;
+use Cake\ORM\RulesChecker;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use Cake\Validation\Validator;
 use MessagingCenter\Model\Table\MessagesTable;
 
 /**
@@ -58,7 +61,10 @@ class MessagesTableTest extends TestCase
      */
     public function testInitialize()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->assertTrue($this->Messages->hasBehavior('Timestamp'), 'Missing behavior Timestamp.');
+        $this->assertInstanceOf('Cake\ORM\Association\BelongsTo', $this->Messages->association('FromUser'));
+        $this->assertInstanceOf('Cake\ORM\Association\BelongsTo', $this->Messages->association('ToUser'));
+        $this->assertInstanceOf(MessagesTable::class, $this->Messages);
     }
 
     /**
@@ -68,7 +74,10 @@ class MessagesTableTest extends TestCase
      */
     public function testValidationDefault()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $validator = new Validator();
+        $result = $this->Messages->validationDefault($validator);
+
+        $this->assertInstanceOf(Validator::class, $result);
     }
 
     /**
@@ -78,6 +87,181 @@ class MessagesTableTest extends TestCase
      */
     public function testBuildRules()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $rules = new RulesChecker();
+        $result = $this->Messages->buildRules($rules);
+
+        $this->assertInstanceOf(RulesChecker::class, $result);
+    }
+
+    public function testGetNewStatus()
+    {
+        $this->assertEquals('new', $this->Messages->getNewStatus());
+    }
+
+    public function testGetReadStatus()
+    {
+        $this->assertEquals('read', $this->Messages->getReadStatus());
+    }
+
+    public function testGetDeletedStatus()
+    {
+        $this->assertEquals('deleted', $this->Messages->getDeletedStatus());
+    }
+
+    public function testGetArchivedStatus()
+    {
+        $this->assertEquals('archived', $this->Messages->getArchivedStatus());
+    }
+
+    public function testGetDateSent()
+    {
+        $this->assertEquals(new Time(), $this->Messages->getDateSent());
+    }
+
+    public function testGetSentFolder()
+    {
+        $this->assertEquals('sent', $this->Messages->getSentFolder());
+    }
+
+    public function testGetDefaultFolder()
+    {
+        $this->assertEquals('inbox', $this->Messages->getDefaultFolder());
+    }
+
+    public function testGetFolders()
+    {
+        $this->assertEquals(['inbox', 'archived', 'sent', 'trash'], $this->Messages->getFolders());
+    }
+
+    public function testFolderExists()
+    {
+        $this->assertTrue($this->Messages->folderExists('inbox'));
+    }
+
+    public function testFolderExistsNot()
+    {
+        $this->assertFalse($this->Messages->folderExists('foo'));
+    }
+
+    public function testGetFolderByMessageFromUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000001');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000001');
+
+        $this->assertEquals('sent', $result);
+    }
+
+    public function testGetFolderByMessageToUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000001');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000002');
+
+        $this->assertEquals('inbox', $result);
+    }
+
+    public function testGetFolderByDeletedMessageFromUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000002');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000001');
+
+        $this->assertEquals('sent', $result);
+    }
+
+    public function testGetFolderByDeletedMessageToUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000002');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000002');
+
+        $this->assertEquals('trash', $result);
+    }
+
+    public function testGetFolderByArchivedMessageFromUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000003');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000001');
+
+        $this->assertEquals('sent', $result);
+    }
+
+    public function testGetFolderByArchivedMessageToUser()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000003');
+        $result = $this->Messages->getFolderByMessage($entity, '00000000-0000-0000-0000-000000000002');
+
+        $this->assertEquals('archived', $result);
+    }
+
+    public function testGetFolderByReferer()
+    {
+        $entity = $this->Messages->get('00000000-0000-0000-0000-000000000001');
+        $userId = '00000000-0000-0000-0000-000000000001';
+        $referer = '/folder/archived';
+        $result = $this->Messages->getFolderByMessage($entity, $userId, $referer);
+
+        $this->assertEquals('archived', $result);
+    }
+
+    public function testGetConditionsByFolderDefault()
+    {
+        $folder = '';
+        $userId = '00000000-0000-0000-0000-000000000001';
+
+        $expected = [
+            'to_user' => $userId,
+            'status IN' => ['read', 'new']
+        ];
+
+        $this->assertEquals($expected, $this->Messages->getConditionsByFolder($userId, $folder));
+    }
+
+    public function testGetConditionsByFolderInbox()
+    {
+        $folder = 'inbox';
+        $userId = '00000000-0000-0000-0000-000000000001';
+
+        $expected = [
+            'to_user' => $userId,
+            'status IN' => ['read', 'new']
+        ];
+
+        $this->assertEquals($expected, $this->Messages->getConditionsByFolder($userId, $folder));
+    }
+
+    public function testGetConditionsByFolderArchived()
+    {
+        $folder = 'archived';
+        $userId = '00000000-0000-0000-0000-000000000001';
+
+        $expected = [
+            'to_user' => $userId,
+            'status' => 'archived'
+        ];
+
+        $this->assertEquals($expected, $this->Messages->getConditionsByFolder($userId, $folder));
+    }
+
+    public function testGetConditionsByFolderSent()
+    {
+        $folder = 'sent';
+        $userId = '00000000-0000-0000-0000-000000000001';
+
+        $expected = [
+            'from_user' => $userId,
+        ];
+
+        $this->assertEquals($expected, $this->Messages->getConditionsByFolder($userId, $folder));
+    }
+
+    public function testGetConditionsByFolderTrash()
+    {
+        $folder = 'trash';
+        $userId = '00000000-0000-0000-0000-000000000001';
+
+        $expected = [
+            'to_user' => $userId,
+            'status' => 'deleted'
+        ];
+
+        $this->assertEquals($expected, $this->Messages->getConditionsByFolder($userId, $folder));
     }
 }
