e4bf6fc77edbb7ee01a080738a4cd2ebd9ae2302
Michael Stepanov <michael@stepanoff.org>
2019-01-29 23:17:21 +0200

    Added controllers and tests for mailboxes and folders - task #8830

---
+++ src/Controller/MailboxesController.php
@@ -0,0 +1,106 @@
+<?php
+namespace MessagingCenter\Controller;
+
+use MessagingCenter\Controller\AppController;
+
+/**
+ * Mailboxes Controller
+ *
+ *
+ * @method \MessagingCenter\Model\Entity\Mailbox[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class MailboxesController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $mailboxes = $this->paginate($this->Mailboxes);
+
+        $this->set(compact('Mailboxes'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Mailbox id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $mailbox = $this->Mailboxes->get($id, [
+            'contain' => []
+        ]);
+
+        $this->set('mailbox', $mailbox);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $mailbox = $this->Mailboxes->newEntity();
+        if ($this->request->is('post')) {
+            $mailbox = $this->Mailboxes->patchEntity($mailbox, $this->request->getData());
+            if ($this->Mailboxes->save($mailbox)) {
+                $this->Flash->success(__('The mailbox has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The mailbox could not be saved. Please, try again.'));
+        }
+        $this->set(compact('mailbox'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Mailbox id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $mailbox = $this->Mailboxes->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $mailbox = $this->Mailboxes->patchEntity($mailbox, $this->request->getData());
+            if ($this->Mailboxes->save($mailbox)) {
+                $this->Flash->success(__('The mailbox has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The mailbox could not be saved. Please, try again.'));
+        }
+        $this->set(compact('mailbox'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Mailbox id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $mailbox = $this->Mailboxes->get($id);
+        if ($this->Mailboxes->delete($mailbox)) {
+            $this->Flash->success(__('The mailbox has been deleted.'));
+        } else {
+            $this->Flash->error(__('The mailbox could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
