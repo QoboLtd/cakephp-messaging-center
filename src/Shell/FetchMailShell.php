16c0e2d216c10b6754bbd4f184acd3f3fa53a043
Leonid Mamchenkov <leonid@mamchenkov.net>
2019-02-23 15:04:57 +0200

    Integrate mail fetching shell with php-imap (task #9063)
    Basic integration of the mail fetching functionality with the
    php-imap library for fetching emails via POP/IMAP.

--- src/Shell/FetchMailShell.php
+++ src/Shell/FetchMailShell.php
@@ -14,9 +14,13 @@ namespace MessagingCenter\Shell;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\ORM\TableRegistry;
+use InvalidArgumentException;
+use MessagingCenter\Enum\IncomingTransportType;
 use MessagingCenter\Enum\MailboxType;
 use MessagingCenter\Model\Entity\Mailbox;
 use NinjaMutex\MutexException;
+use PhpImap\ConnectionException;
+use PhpImap\Mailbox as RemoteMailbox;
 use Qobo\Utils\Utility\Lock\FileLock;
 
 class FetchMailShell extends Shell
@@ -71,6 +75,73 @@ class FetchMailShell extends Shell
      */
     protected function processMailbox(Mailbox $mailbox): void
     {
-        $this->out('Fetching mail for [' . $mailbox->name . ']');
+        $defaultSettings = [
+            'username' => '',
+            'password' => '',
+            'host' => 'localhost',
+            'port' => null,
+            'protocol' => 'imap',
+        ];
+
+        try {
+            $this->out('Fetching mail for [' . $mailbox->name . ']');
+
+            $settings = json_decode($mailbox->incoming_settings, true) ?? [];
+            $settings = array_merge($defaultSettings, $settings);
+
+            $connectionString = $this->getConnectionString($mailbox->incoming_transport, $settings);
+            $this->out("Connection: $connectionString");
+            $mailbox = new RemoteMailbox($connectionString, $settings['username'], $settings['password']);
+
+            $messageIds = $mailbox->searchMailbox('ALL');
+            if (empty($messageIds)) {
+                return;
+            }
+
+            foreach ($messageIds as $messageId) {
+                $message = $mailbox->getMail($messageId);
+                debug($message);
+
+                return;
+            }
+        } catch (InvalidArgumentException $e) {
+            $this->warn($e->getMessage());
+        } catch (ConnectionException $e) {
+            $this->warn($e->getMessage());
+        }
+    }
+
+    /**
+     * Build connection string
+     *
+     * Example: {localhost:993/imap/notls}INBOX
+     *
+     * @param string $type Incoming transport type
+     * @param mixed[] $settings Incoming transport settings
+     * @return string
+     */
+    protected function getConnectionString(string $type, array $settings): string
+    {
+        $result = '';
+
+        switch ($type) {
+            case (string)IncomingTransportType::IMAP4():
+                // See more details at http://php.net/manual/en/function.imap-open.php
+                $result .= '{';
+                $result .= $settings['host'] ?? 'localhost';
+                $result .= ':' . ($settings['port'] ?? 993);
+                $result .= '/' . ($settings['protocol'] ?? 'imap');
+                // TODO: Make this optional
+                $result .= '/notls';
+                $result .= '}';
+                // TODO: Make this flexible
+                $result .= 'INBOX';
+
+                break;
+            default:
+                throw new InvalidArgumentException("Incoming transport type [$type] is not supported");
+        }
+
+        return $result;
     }
 }
