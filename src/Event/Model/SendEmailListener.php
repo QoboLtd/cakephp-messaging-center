7a35c08b40b821ee6fd3bd7a4e4b9cfdc2e3241c
Michael Stepanov <michael@stepanoff.org>
2019-03-14 08:30:12 +0200

    Added new listener to send email - task #8830

---
+++ src/Event/Model/SendEmailListener.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace MessagingCenter\Event\Model;
+
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\Mailer\Email;
+use Cake\Utility\Inflector;
+use MessagingCenter\Event\EventName;
+
+class SendEmailListener implements EventListenerInterface
+{
+    /**
+     * implementedEvents method
+     *
+     * @return mixed[]
+     */
+    public function implementedEvents() : array
+    {
+        return [
+            (string)EventName::SEND_EMAIL() => 'sendEmail',
+        ];
+    }
+
+    /**
+     * sendEmail method
+     *
+     * @param \Cake\Event\Event $event event.
+     * @param \Cake\Datasource\EntityInterface $mailbox to get config from
+     * @param mixed[] $data to build email
+     * @return void
+     */
+    public function sendEmail(Event $event, EntityInterface $mailbox, array $data) : void
+    {
+        $outgoingSettings = json_decode($mailbox->get('outgoing_settings'), true);
+
+        Email::configTransport('custom', [
+            'host' => (!empty($outgoingSettings['use_ssl']) ? 'ssl://' : '') . $outgoingSettings['host'],
+            'port' => $outgoingSettings['port'],
+            'username' => $outgoingSettings['username'],
+            'password' => $outgoingSettings['password'],
+            'className' => Inflector::camelize($mailbox->get('outgoing_transport'))
+        ]);
+
+        /**
+         * @var \Cake\Mailer\Email $email
+         */
+        $email = new Email('default');
+        $result = $email->from($outgoingSettings['username'])
+            ->setTransport('custom')
+            ->to($data['to_user'])
+            ->subject($data['subject'])
+            ->send($data['content']);
+    }
+}
