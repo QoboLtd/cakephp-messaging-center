08fc4775ad44c1440da54d35c4f129d5f8f6e0b5
Michael Stepanov <michael@stepanoff.org>
2019-04-02 20:10:03 +0300

    Added test for Mailbox Listener - task #8830

---
+++ tests/TestCase/Event/Model/MailboxListenerTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace MessagingCenter\Test\TestCase\Event\Model;
+
+use Cake\Core\Configure;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventList;
+use Cake\Event\EventManager;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use MessagingCenter\Event\Model\MailboxListener;
+
+class MailboxListenerTest extends TestCase
+{
+    public $fixtures = [
+        'plugin.CakeDC/Users.users',
+        'plugin.MessagingCenter.mailboxes',
+        'plugin.MessagingCenter.folders',
+        'plugin.MessagingCenter.messages',
+    ];
+
+    /**
+     * @var \MessagingCenter\Model\Table\Mailboxes $mailboxes
+     */
+    protected $mailboxes;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Configure::write('Users.table', 'CakeDC/Users.Users');
+
+        $this->mailboxes = TableRegistry::get('MessagingCenter.Mailboxes');
+
+        // enable event tracking
+        $this->mailboxes->getEventManager()->setEventList(new EventList());
+        EventManager::instance()->on(new MailboxListener());
+    }
+
+    public function tearDown()
+    {
+        unset($this->mailboxes);
+
+        parent::tearDown();
+    }
+
+    /**
+     * testCreateFolders method
+     *
+     * @return void
+     */
+    public function testCreateFolders() : void
+    {
+        $data = [
+            'user_id' => '00000000-0000-0000-0000-000000000001',
+            'name' => 'mytest@system',
+            'type' => 'system',
+            'incoming_transport' => 'internal',
+            'incoming_settings' => 'default',
+            'outgoing_transport' => 'internal',
+            'outgoing_settings' => 'default',
+            'active' => 1,
+        ];
+
+        $mailbox = $this->mailboxes->newEntity();
+        $this->mailboxes->patchEntity($mailbox, $data);
+        $result = $this->mailboxes->save($mailbox, $data);
+
+        $this->assertEventFired('Model.afterSave', $this->mailboxes->getEventManager());
+
+        $this->assertInstanceOf(EntityInterface::class, $result, 'Cannot create a new mailbox');
+    }
+}
