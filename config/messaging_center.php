8bfde998334f6a69fe3096682eb400a7dfaf95bd
Iacovos Constantinou <softius@gmail.com>
2019-07-31 16:28:39 +0300

    Remove typeahead script (task #12642)

--- config/messaging_center.php
+++ config/messaging_center.php
@@ -6,13 +6,6 @@ return [
             'id' => '00000000-0000-0000-0000-000000000000',
             'name' => 'SYSTEM'
         ],
-        'typeahead' => [
-            'min_length' => 1,
-            'timeout' => 300
-        ],
-        'api' => [
-            'token' => null
-        ],
         'welcomeMessage' => [
             'enabled' => true,
             'projectName' => 'Project Name',
