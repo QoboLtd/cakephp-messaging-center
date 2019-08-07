e3b6048631778ade10480c9bcca87c1ac2a6f41e
Iacovos Constantinou <softius@gmail.com>
2019-08-07 22:55:57 +0300

    Add MessageFactory (task #12848)

--- src/Shell/FetchMailShell.php
+++ src/Shell/FetchMailShell.php
@@ -14,21 +14,17 @@ namespace MessagingCenter\Shell;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use Cake\Core\Configure;
-use Cake\Datasource\EntityInterface;
 use Cake\ORM\Exception\PersistenceFailedException;
-use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
-use DateTime;
 use Exception;
 use InvalidArgumentException;
 use MessagingCenter\Enum\MailboxType;
 use MessagingCenter\Model\Entity\Mailbox;
+use MessagingCenter\Model\MessageFactory;
 use MessagingCenter\Model\Table\MailboxesTable;
-use MessagingCenter\Model\Table\MessagesTable;
 use NinjaMutex\MutexException;
 use PhpImap\Exceptions\ConnectionException;
 use PhpImap\Exceptions\InvalidParameterException;
-use PhpImap\IncomingMail;
 use PhpImap\Mailbox as RemoteMailbox;
 use Qobo\Utils\Utility\Lock\FileLock;
 use Webmozart\Assert\Assert;
@@ -164,7 +160,8 @@ class FetchMailShell extends Shell
                 /** @var \PhpImap\IncomingMail $message */
                 $message = $remoteMailbox->getMail($messageHeader->uid, $markAsSeenRemote);
 
-                $this->saveMessage($message, $mailbox);
+                // $this->saveMessage($message, $mailbox);
+                MessageFactory::fromIncomingMail($message, $mailbox);
                 $this->out(sprintf('Message %s saved', trim($messageHeader->message_id)));
             } catch (InvalidArgumentException | PersistenceFailedException $e) {
                 $this->err(sprintf('Message %s can not be saved. %s', trim($messageHeader->message_id), $e->getMessage()));
@@ -195,100 +192,5 @@ class FetchMailShell extends Shell
 
             throw $e;
         }
-    }
-
-    /**
-     * Saves the message into the database, under the specified mailbox.
-     *
-     * @param \PhpImap\IncomingMail $message received from the mailbox
-     * @param \Cake\Datasource\EntityInterface $mailbox to save message
-     * @return void
-     */
-    protected function saveMessage(IncomingMail $message, EntityInterface $mailbox) : void
-    {
-        $mailboxes = TableRegistry::getTableLocator()->get('MessagingCenter.Mailboxes');
-        Assert::isInstanceOf($mailboxes, MailboxesTable::class);
-
-        $table = TableRegistry::getTableLocator()->get('MessagingCenter.Messages');
-        Assert::isInstanceOf($table, MessagesTable::class);
-
-        $content = $message->textPlain ?? $message->textHtml;
-
-        /**
-         * Retrieve initialStatus for local saved email from configuration.
-         * @TODO Put this into database while setting up mailbox
-         * @var string
-         */
-        $initialStatus = (string)Configure::read('MessagingCenter.local_mailbox_messages.initialStatus', 'new');
-
-        $entity = $table->newEntity();
-        $table->patchEntity($entity, [
-            'subject' => $message->subject,
-            'content' => $content,
-            'status' => $initialStatus,
-            'date_sent' => $this->extractDateTime($message),
-            'from_user' => '',
-            'from_name' => '',
-            'to_user' => '',
-            'headers' => $message->headers,
-            'message_id' => $message->messageId,
-            'folder_id' => $mailboxes->getInboxFolder($mailbox),
-        ]);
-
-        $messageCreated = $table->saveOrFail($entity);
-        $this->saveAttachments($messageCreated, $message->getAttachments());
-    }
-
-    /**
-     * Saves and links attachments with the specified message
-     *
-     * @param \Cake\Datasource\EntityInterface $message Message to be associated with attachmetns
-     * @param \PhpImap\IncomingMailAttachment[] $attachments Attachments to be saved
-     */
-    public function saveAttachments(EntityInterface $message, array $attachments): void
-    {
-        $storageTable = TableRegistry::get('Burzum/FileStorage.FileStorage');
-        foreach ($attachments as $attachment) {
-            $storage = $storageTable->newEntity([
-                'file' => [
-                    'tmp_name' => $attachment->filePath,
-                    'error' => 0,
-                    'name' => $attachment->name,
-                    'type' => null,
-                    'size' => null,
-                ]
-            ]);
-
-            $storage = $storageTable->patchEntity($storage, [
-                'model' => 'Messages',
-                'model_field' => 'attachments',
-                'foreign_key' => $message->get('id'),
-                'extension' => strtolower($storage->get('extension')),
-            ]);
-
-            $storageTable->saveOrFail($storage);
-        }
-    }
-
-    /**
-     * Extracts the DateTime from the provided message
-     *
-     * @param mixed $message Email message object
-     * @return \DateTime
-     */
-    protected function extractDateTime($message): DateTime
-    {
-        if (property_exists($message, 'udate')) {
-            $dateSent = new DateTime($message->udate);
-        } else {
-            $dateSent = DateTime::createFromFormat('Y-m-d H:i:s', $message->date);
-        }
-
-        $errors = DateTime::getLastErrors();
-        if ($dateSent !== false && empty($errors['warning_count'])) {
-            return $dateSent;
-        }
-
-        return new DateTime();
     }
 }
