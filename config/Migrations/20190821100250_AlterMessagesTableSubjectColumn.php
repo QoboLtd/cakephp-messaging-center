229170e283f74398230cb9fad9cabf05d33a78e2
George Constantinou <georgec20001@gmail.com>
2019-08-21 13:08:41 +0300

    Set subject column as nullable (task #12077)

---
+++ config/Migrations/20190821100250_AlterMessagesTableSubjectColumn.php
@@ -0,0 +1,25 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AlterMessagesTableSubjectColumn extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('qobo_messages');
+
+        $table->changeColumn('subject', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true
+        ]);
+
+        $table->update();
+    }
+}
