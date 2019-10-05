dd354a123bcb21022dff9d5a96fe318550c211e9
Iacovos Constantinou <softius@gmail.com>
2019-10-05 22:03:44 +0300

    Extend Store interface (task #12075)

--- src/Store/StoreInterface.php
+++ src/Store/StoreInterface.php
@@ -1,11 +1,21 @@
 <?php
 
-namespace MessagingCenter\Transport;
+namespace MessagingCenter\Store;
 
 interface StoreInterface
 {
     /**
+     * @param int|null $limit How many messages to return
+     * @param int $offset How many rows to skip before beginning to return messages
      * @return \MessagingCenter\Model\Entity\Message[]
      */
-    public function getMessages(): array;
+    public function getMessages(int $limit = null, int $offset = 0): array;
+
+    /**
+     * @param string[] $criteria Search criteria
+     * @param int|null $limit How many messages to return
+     * @param int $offset How many rows to skip before beginning to return messages
+     * @return \MessagingCenter\Model\Entity\Message[]
+     */
+    public function searchMessages(array $criteria, int $limit = null, int $offset = 0): array;
 }
