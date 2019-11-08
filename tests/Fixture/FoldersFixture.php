86863ecf764b0bfe95ecef6084623e701c66a41a
George Constantinou <georgec20001@gmail.com>
2019-11-08 17:20:13 +0200

    More fixture fields adjustments (task #14037)

--- tests/Fixture/FoldersFixture.php
+++ tests/Fixture/FoldersFixture.php
@@ -21,10 +21,10 @@ class FoldersFixture extends TestFixture
         'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'mailbox_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'parent_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'type' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'icon' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'order_no' => ['type' => 'integer', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'type' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'icon' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'order_no' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
@@ -32,7 +32,7 @@ class FoldersFixture extends TestFixture
         ],
         '_options' => [
             'engine' => 'InnoDB',
-            'collation' => 'utf8mb4_unicode_ci'
+            'collation' => 'utf8mb4_general_ci'
         ],
     ];
     // @codingStandardsIgnoreEnd
@@ -51,9 +51,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Sent',
                 'type' => 'default',
+                'order_no' => 10,
                 'created' => '2019-01-07 20:47:29',
                 'modified' => '2019-01-07 20:47:29',
-                'order_no' => 10,
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000002',
@@ -61,9 +61,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Inbox',
                 'type' => 'default',
+                'order_no' => 1,
                 'created' => '2019-01-07 20:47:29',
                 'modified' => '2019-01-07 20:47:29',
-                'order_no' => 1,
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000003',
@@ -71,8 +71,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Inbox',
                 'type' => 'default',
+                'order_no' => 2,
                 'created' => '2019-01-07 20:47:29',
-                'modified' => '2019-01-07 20:47:29'
+                'modified' => '2019-01-07 20:47:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000004',
@@ -80,8 +81,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Inbox',
                 'type' => 'default',
+                'order_no' => 3,
                 'created' => '2019-01-07 20:47:29',
-                'modified' => '2019-01-07 20:47:29'
+                'modified' => '2019-01-07 20:47:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000005',
@@ -89,8 +91,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Sent',
                 'type' => 'default',
+                'order_no' => 11,
                 'created' => '2019-01-07 20:47:29',
-                'modified' => '2019-01-07 20:47:29'
+                'modified' => '2019-01-07 20:47:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000006',
@@ -98,8 +101,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Trash',
                 'type' => 'default',
+                'order_no' => 13,
                 'created' => '2019-01-07 20:47:29',
-                'modified' => '2019-01-07 20:47:29'
+                'modified' => '2019-01-07 20:47:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000007',
@@ -107,8 +111,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Archive',
                 'type' => 'default',
+                'order_no' => 12,
                 'created' => '2019-01-07 20:47:29',
-                'modified' => '2019-01-07 20:47:29'
+                'modified' => '2019-01-07 20:47:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000008',
@@ -116,8 +121,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Trash',
                 'type' => 'default',
+                'order_no' => 23,
                 'created' => '2019-10-16 16:14:29',
-                'modified' => '2019-10-16 16:14:29'
+                'modified' => '2019-10-16 16:14:29',
             ],
             [
                 'id' => '00000000-0000-0000-0000-000000000009',
@@ -125,8 +131,9 @@ class FoldersFixture extends TestFixture
                 'parent_id' => '',
                 'name' => 'Archive',
                 'type' => 'default',
+                'order_no' => 17,
                 'created' => '2019-10-16 16:28:29',
-                'modified' => '2019-10-16 16:28:29'
+                'modified' => '2019-10-16 16:28:29',
             ],
         ];
         parent::init();
